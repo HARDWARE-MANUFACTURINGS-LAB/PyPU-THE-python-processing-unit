78c32ca6d1e6a5aebeeef6f90ed035fda587ca7c
한진희 <0200134hjh@gmail.com>
2026-02-03 15:18:10 +0900

    Refactor PyPU_Commander for new architectures

--- ARCH/Main.py
+++ ARCH/Main.py
@@ -1,41 +1,29 @@
 import sys
 from ARCH.ARDUINO import PyPU_AVR
 from ARCH.Esp32 import PyPU_ESP32
-from ARCH.Stm32nucleo import PyPU_NUCLEO
+from ARCH.Stm32nucleo import PyPU_ARM
+from ARCH.Amd64 import PyPU_X64
 
 class PyPU_Commander:
-    def __init__(self, target, port):
+    def __init__(self, target, port=None):
         self.target = target.upper()
         self.port = port
         self.engines = {
             'AVR': PyPU_AVR,
             'ESP32': PyPU_ESP32,
-            'NUCLEO': PyPU_NUCLEO
+            'ARM': PyPU_ARM,
+            'X64': PyPU_X64
         }
 
     def deploy(self, source_code):
         if self.target not in self.engines:
-            print(f"[!] Target {self.target} not supported.")
             return
-
-        print(f"[*] Deploying to {self.target} via {self.port}...")
-        engine = self.engines[self.target](self.port)
+        
+        engine = self.engines[self.target](self.port) if self.port else self.engines[self.target]()
         instructions = engine.compile(source_code)
-        engine.flash(instructions)
-        print(f"[*] {self.target} synchronized successfully.")
+        engine.execute_or_flash(instructions)
 
 if __name__ == "__main__":
-    target_board = 'NUCLEO'
-    comm_port = 'COM4'
-    
-    logic = """
-pin_mode(13, 1)
-while True:
-    digital_write(13, 1)
-    delay(500)
-    digital_write(13, 0)
-    delay(500)
-"""
-    
-    commander = PyPU_Commander(target_board, comm_port)
-    commander.deploy(logic)
+    target_arch = 'X64' 
+    commander = PyPU_Commander(target_arch)
+    commander.deploy("rax = 1\nsyscall()")
