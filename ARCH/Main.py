890a0e049371721593c259f25ac924ba64f031fd
한진희 <0200134hjh@gmail.com>
2026-02-03 15:38:39 +0900

    Add RISC-V support to PyPU_Commander

--- ARCH/Main.py
+++ ARCH/Main.py
@@ -3,6 +3,7 @@ from ARCH.ARDUINO import PyPU_AVR
 from ARCH.Esp32 import PyPU_ESP32
 from ARCH.Stm32nucleo import PyPU_ARM
 from ARCH.Amd64 import PyPU_X64
+from ARCH.RISCV import PyPU_RISCV
 
 class PyPU_Commander:
     def __init__(self, target, port=None):
@@ -12,7 +13,8 @@ class PyPU_Commander:
             'AVR': PyPU_AVR,
             'ESP32': PyPU_ESP32,
             'ARM': PyPU_ARM,
-            'X64': PyPU_X64
+            'X64': PyPU_X64,
+            'RISCV': PyPU_RISCV
         }
 
     def deploy(self, source_code):
@@ -24,6 +26,7 @@ class PyPU_Commander:
         engine.execute_or_flash(instructions)
 
 if __name__ == "__main__":
-    target_arch = 'X64' 
+    
+    target_arch = 'RISCV' 
     commander = PyPU_Commander(target_arch)
-    commander.deploy("rax = 1\nsyscall()")
+    commander.deploy("x10 = 5\nx11 = 10")
